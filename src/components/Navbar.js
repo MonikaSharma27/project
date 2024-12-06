c6d95c57fb44c8e2411e45a847b5a029d23fc1cd
Monika Sharma <monikasharma279826@gmail.com>
2024-12-06 19:07:11 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import image from "../images/homeimg.webp"
+import image from "../images/homeimg.webp";
+import image1 from "../images/returnimg.jpg"
+import image2 from "../images/freeshiping.png"
+import image3 from "../images/cod.png"
 
 function Navbar() {
   return (
@@ -18,6 +21,23 @@ function Navbar() {
 
         <main>
            <img src={image} className='border-t-2 border-t-slate-200 pl-10 w-full pr-10'></img>
+           <div className='flex items-start justify-center gap-20 mt-3 '>
+            <div >
+            <img src={image1} className='w-20 ' ></img>
+            <h2 className='font-bold text-gray-600 text-center text-2xl'>15 Days Free</h2>
+            <p className='text-center text-gray-600'>Return</p>
+            </div>
+            <div>
+            <img src={image2} className='w-20' ></img>
+            <h2 className='font-bold text-gray-600 text-center text-2xl'>Free Shipping</h2>
+            <p className='text-center text-gray-600'>Across India</p>
+            </div>
+            <div>
+            <img src={image3} className='w-20' ></img>
+            <h2 className='font-bold text-gray-600 text-center text-2xl'>COD</h2>
+            <p className='text-center text-gray-600'>Available</p>
+            </div>
+           </div>
 
         </main>
         
