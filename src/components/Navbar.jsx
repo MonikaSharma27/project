07588544e17f1ad791bcbb4e09e1490c0d8074e4
Monika Sharma <monikasharma279826@gmail.com>
2024-12-07 15:01:23 +0530

    changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,23 @@ function Navbar() {
     <div >
         <nav className='navbar flex'>
             <ul className='flex p-5'>
-                <li className='pl-12 text-1xl'>Home </li>
-                <li className='pl-12 text-1xl'>Necklaces </li>
-                <li className='pl-12 text-1xl'>Earrings </li>
-                <li className='pl-12 text-1xl'>Bangles </li>
-                <li className='pl-12 text-1xl'>Collection </li>
-                <li className='pl-12 text-1xl'>Wedding Store </li>
-                <li className='pl-12 text-1xl'>Happy Customers </li>
+                <li className='pl-12 text-1xl'>
+                  <a>Home</a>
+                  <div className='bg-gray-200 w-full hidden z-[-1] p-4 w-48 mt-5 hover:block z-40 '>
+                   <ul>
+                    <li><a>Bridal Sets</a></li>
+                    <li><a>Short Necklaces</a></li>
+                    <li><a>Pair Bangle</a></li>
+                    <li><a>Bracelet</a></li>
+                   </ul>
+                  </div>
+                  </li>
+                <li className='pl-12 text-1xl'><a>Necklaces </a></li>
+                <li className='pl-12 text-1xl'><a>Earrings</a> </li>
+                <li className='pl-12 text-1xl'><a>Bangles</a> </li>
+                <li className='pl-12 text-1xl'><a>Collection </a></li>
+                <li className='pl-12 text-1xl'><a>Wedding Store </a></li>
+                <li className='pl-12 text-1xl'><a>Happy Customers </a></li>
             </ul>
         </nav>
 
@@ -23,17 +33,17 @@ function Navbar() {
            <img src={image} className='border-t-2 border-t-slate-200 pl-10 w-full pr-10'></img>
            <div className='flex items-start justify-center gap-20 mt-3 '>
             <div >
-            <img src={image1} className='w-20 ' ></img>
+            <img src={image1} className='w-20 mx-auto' ></img>
             <h2 className='font-bold text-gray-600 text-center text-2xl'>15 Days Free</h2>
             <p className='text-center text-gray-600'>Return</p>
             </div>
             <div>
-            <img src={image2} className='w-20' ></img>
+            <img src={image2} className='w-20 mx-auto' ></img>
             <h2 className='font-bold text-gray-600 text-center text-2xl'>Free Shipping</h2>
             <p className='text-center text-gray-600'>Across India</p>
             </div>
             <div>
-            <img src={image3} className='w-20' ></img>
+            <img src={image3} className='w-20 mx-auto' ></img>
             <h2 className='font-bold text-gray-600 text-center text-2xl'>COD</h2>
             <p className='text-center text-gray-600'>Available</p>
             </div>
